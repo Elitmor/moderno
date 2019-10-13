4034aac376b2f888c39aa2608df9ab760d80a458
Vitaly <elitmor@gmail.com>
2019-10-13 12:22:20 +0300

    revision of the project

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,15 @@ let gulp = require('gulp'),
     browserSync = require('browser-sync'),
     autoprefixer = require('gulp-autoprefixer'),
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglify');
-cssmin = require('gulp-cssmin');
-
+    uglify = require('gulp-uglify'),
+    cssmin = require('gulp-cssmin');
 
 gulp.task('sass', function () {
     return gulp.src('app/scss/**/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(autoprefixer({
-            overrideBrowserslist: ['last 8 version']
+            overrideBrowserslist: ['last 8 versions']
         }))
         .pipe(gulp.dest('app/css'))
         .pipe(browserSync.reload({ stream: true }))
@@ -26,7 +25,10 @@ gulp.task('style', function () {
         'node_modules/magnific-popup/dist/magnific-popup.css',
         'node_modules/rateyo/src/jquery.rateyo.css',
         'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
-        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css'
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css'
+
     ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -40,7 +42,8 @@ gulp.task('script', function () {
         'node_modules/mixitup/dist/mixitup.js',
         'node_modules/rateyo/src/jquery.rateyo.js',
         'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
-        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js'
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
@@ -48,28 +51,27 @@ gulp.task('script', function () {
 });
 
 gulp.task('html', function () {
-    return gulp.src('app/*html')
+    return gulp.src('app/*.html')
         .pipe(browserSync.reload({ stream: true }))
 });
 
-
 gulp.task('js', function () {
-    return gulp.src('app/*html')
+    return gulp.src('app/js/*.js')
         .pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('browser-sync', function () {
     browserSync.init({
         server: {
-            baseDir: 'app/'
+            baseDir: "app/"
         }
     });
 });
 
 gulp.task('watch', function () {
-    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
-    gulp.watch('app/*.html', gulp.parallel('html'));
-    gulp.watch('app/js/*.js', gulp.parallel('js'));
-})
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
+    gulp.watch('app/*.html', gulp.parallel('html'))
+    gulp.watch('app/js/*.js', gulp.parallel('js'))
+});
 
 gulp.task('default', gulp.parallel('style', 'script', 'sass', 'watch', 'browser-sync'))
